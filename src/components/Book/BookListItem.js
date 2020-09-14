51c1108ec5d722cdc72475af7b260101265084e7
developercrown <ingeniero.rene.corona@gmail.com>
2020-09-13 22:38:24 -0500

    third commit

--- src/components/Book/BookListItem.js
+++ src/components/Book/BookListItem.js
@@ -1,22 +1,90 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {Image, View, Text, StyleSheet} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
+import Icon from 'react-native-ionicons';
+import {startClock} from 'react-native-reanimated';
 
 const styles = StyleSheet.create({
-    row: {
-        marginBottom: 4,
-        borderBottomColor: 'rgba(0,0,0,0.1)',
-        borderBottomWidth: 1,
-        padding: 6
-    }
-})
+  row: {
+    marginBottom: 4,
+    borderBottomColor: 'rgba(0,0,0,0.1)',
+    borderBottomWidth: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    flex: 1,
+    paddingVertical: 4,
+    paddingLeft: 4
+  },
+  firstRow: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    width: 50,
+    marginRight: 8,
+  },
+  secondRow: {
+    flexDirection: 'column',
+    flex: 7,
+  },
 
-const BookListItem = ({book}) => {
-    return (
-        <View style={styles.row}>
-            <Text>{book.title}</Text>
+  thirdRow: {
+    alignItems: 'center',
+    flexDirection: 'column',
+    flex: 1,
+    height: '100%',
+    paddingRight: 10,
+    paddingTop: 8
+  },
+
+  contentRow: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    padding: 0,
+    margin: 0
+  },
+
+  image: {
+    width: 50,
+    height: 50,
+    resizeMode: 'cover',
+    alignSelf: 'center',
+  },
+  icon: {
+    color: '#000',
+    marginLeft: 'auto',
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  noMargin: {
+      margin: 0,
+      padding: 0,
+      backgroundColor: '#f00'
+  }
+});
+
+const BookListItem = ({book, onPress}) => {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View style={styles.row}>
+        <View style={styles.firstRow}>
+          <Image source={{uri: book.cover}} style={styles.image} />
         </View>
-    )
-}
+        <View style={styles.secondRow}>
+          <View style={styles.contentRow}>
+            <Text>{book.title}</Text>
+          </View>
+          <View style={styles.contentRow}>
+            <Icon name="library-outline" />
+            <Text style={styles.boldText}>Author: {book.author}</Text>
+          </View>
+        </View>
+        <View style={styles.thirdRow}>
+          <Icon name="arrow-dropright" style={styles.icon} />
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
 export default BookListItem;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,46 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import {View, Text, TouchableHighlight, StyleSheet} from 'react-native';
 import {FlatList} from 'react-native-gesture-handler';
-
 import BookListItem from '../components/Book/BookListItem';
 
-const styles = StyleSheet.create({
-    results: {
-        padding: 10
-    },
-    rowHeader: {
-      marginBottom: 10,
-      backgroundColor: 'rgba(0,0,0,0.05)',
-      padding: 6
-  }
-})
+import { useQuery } from 'react-query'
 
-const BOOK_LIST = [
-  {
-    id: 1,
-    title: 'El cid',
+const styles = StyleSheet.create({
+  results: {
+    padding: 10,
   },
-  {
-    id: 2,
-    title: 'Un mexicano más',
+  rowHeader: {
+    marginBottom: 10,
+    backgroundColor: 'rgba(0,0,0,0.05)',
+    padding: 6,
   },
-  {
-      id: 3,
-      title: 'Cosmos'
-  }
-];
+});
+
+const GET_BOOKS = 'GET_BOOKS';
+
+async function fetchData() {
+  // const response = await fetch('http://archivos.upn164.edu.mx/develop/books.json');
+  const response = await fetch('https://www.etnassoft.com/api/v1/get/?category=libros_programacion');
+  const json = await response.json();
+  return json;
+}
+
 
 const Home = ({navigation}) => {
+  const handleOnPress = () => {
+    navigation.navigate('Library');
+  }
+
+  const {status, data, error} = useQuery(GET_BOOKS, fetchData);
+  console.log(data);
+
   return (
     <View style={styles.results}>
       <FlatList
-            data={BOOK_LIST}
-            renderItem={({item}) => <BookListItem book={item} />}
-            keyExtractor={item => item.id}
-            ListHeaderComponent={<View style={styles.rowHeader}><Text>Mi lista de libros</Text></View>}
-        >
-
-        </FlatList>
+        data={status === 'success' ? data : []}
+        renderItem={({item}) => <BookListItem book={item} onPress={handleOnPress}/>}
+        keyExtractor={(item, index) => `list-item${item}${index}`}
+        ListHeaderComponent={
+          <View style={styles.rowHeader}>
+            <Text>Mi lista de libros</Text>
+          </View>
+        }
+        ListEmptyComponent={
+          <View>
+            {
+              status === 'loading' && <View><Text>Cargando Libros...</Text></View>
+            }
+          </View>
+        }
+        ></FlatList>
     </View>
   );
 };
